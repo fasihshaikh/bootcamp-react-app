a91770a60660c670f5f9d298e604df98fa9bc9d6
Fasih Ahmed Shaikh <fasihshaikh@hotmail.com>
2021-01-19 16:08:43 +0500

    created one dinner.js file and updated App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Dinner from './dinner.js';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World from fasih</h1>
+       <Dinner />
     </div>
   );
 }
---
+++ src/dinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Dinner (){
+    return(
+        <div>
+            <h1>Hello World</h1>
+            <h1>I love food</h1>
+        </div>
+    )
+}
+export default Dinner;
